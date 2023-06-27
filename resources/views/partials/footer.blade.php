2abbca9f972c9f6921e9f2e7fb6e7141855b6158
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-06-27 17:23:30 +0200

    imported footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,70 @@
-<footer class="p-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</footer>
+<footer>
+    
+    <div class="footer-top">
+        <div class="container pos-relative">
+            <div class="d-flex">
+                <div class="footer-top-column">
+                    <strong class="footer-list-title">DC COMICS</strong>
+                    <ul>
+                        <li><small><a href="">Characters</a></small></li>
+                        <li><small><a href="">Comics</a></small></li>
+                        <li><small><a href="">Movies</a></small></li>
+                        <li><small><a href="">TV</a></small></li>
+                        <li><small><a href="">Games</a></small></li>
+                        <li><small><a href="">Videos</a></small></li>
+                        <li><small><a href="">News</a></small></li>
+                    </ul>
+                    <strong class="footer-list-title">SHOP</strong>
+                    <ul>
+                        <li><small><a href="">Shop DC</a></small></li>
+                        <li><small><a href="">Shop DC Collectibles</a> </small></li>
+                    </ul>
+                </div>
+                <div class="footer-top-column">
+                    <strong class="footer-list-title">DC</strong>
+                    <ul>
+                        <li><small><a href="">Terms Of Use</a></small></li>
+                        <li><small><a href="">Privacy policy (New)</a></small></li>
+                        <li><small><a href="">Ad Choices</a></small></li>
+                        <li><small><a href="">Advertising</a></small></li>
+                        <li><small><a href="">Jobs</a> </small></li>
+                        <li><small><a href="">Subscriptions</a></small></li>
+                        <li><small><a href="">Talent Workshops</a></small></li>
+                        <li><small><a href="">CPSC Certificates</a></small></li>
+                        <li><small><a href="">Ratings</a></small></li>
+                        <li><small><a href="">Shop Help</a></small></li>
+                        <li><small><a href="">Contact Us</a></small></li>
+                    </ul>
+                </div>
+                <div class="footer-top-column">
+                    <strong class="footer-list-title">SITES</strong>
+                    <ul>
+                        <li><small><a href="">DC</a></small></li>
+                        <li><small><a href="">MAD Magazine</a></small></li>
+                        <li><small><a href="">DC Kids</a></small></li>
+                        <li><small><a href="">DC Universe</a></small></li>
+                        <li><small><a href="">DC Power Visa</a></small></li>
+                    </ul>
+                </div>
+            </div>
+            <img class="pos-absolute" src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+        </div> 
+    </div>
+
+    <div class="footer-bottom">
+        <div class="container d-flex justify-content-between align-items-center">
+            <div>
+                <button><strong>SIGN-UP NOW!</strong></button>
+            </div>
+            <div class="d-flex align-items-center social-link-container">
+                <h4>FOLLOW US</h4>
+                <img src="../assets/img/footer-facebook.png" alt="">
+                <img src="../assets/img/footer-twitter.png" alt="">
+                <img src="../assets/img/footer-youtube.png" alt="">
+                <img src="../assets/img/footer-pinterest.png" alt="">
+                <img src="../assets/img/footer-periscope.png" alt="">
+            </div>
+        </div>
+    </div>
+
+</footer>
